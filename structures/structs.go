fb03050557ead5d5ad25814d3d22155eeeae7138
viswamvs <viswavickymvs@gmail.com>
2022-05-11 11:30:17 +0530

    Updated structures

--- structures/structs.go
+++ structures/structs.go
@@ -1,13 +1,21 @@
-package main
+package structures
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //Declaring struct type
+type Salary struct {
+	basic int
+	insurance int
+	allowance int
+}
 type Employee struct {
 	firstName, lastName string
-	salary int
+	Salary //nested struct
 	fullTime bool
 }
+
 
 func main(){
 	//creating struct (here employee is a struct)
@@ -16,7 +24,7 @@ func main(){
 	//setting values to struct fields (initializing struct)
 	employee.firstName = "viswa"
 	employee.lastName = "m"
-	employee.salary = 1200
+	employee.Salary = Salary{1000, 200, 300}
 	employee.fullTime = true
 
 	//getting values from struct fields
@@ -28,14 +36,14 @@ func main(){
 	emp := Employee{
 		firstName: "arun",
 		lastName: "kumar",
-		salary: 1000,
+		Salary: Salary{1200, 100, 2000},
 		fullTime: false,
 	}
 	fmt.Println(emp)
 
 	//another way of initializing struct
 	//here it's mandatory to initialize all fields
-	em := Employee{"dinesh", "ravi", 100, false}
+	em := Employee{"dinesh", "ravi", Salary{1000, 200, 100}, false}
 	fmt.Println(em)
 	fmt.Printf("%T", em)
 	fmt.Println()
@@ -47,7 +55,6 @@ func main(){
 	}{
 		firstName: "nikhil",
 		lastName: "saji",
-		
 	}
 	fmt.Println(employee1)
 	fmt.Printf("%T", employee1)
@@ -58,7 +65,7 @@ func main(){
 		firstName: "viswa",
 		lastName: "m",
 		fullTime: true,
-		salary: 1000,
+		Salary: Salary{2000, 0, 200},
 	}
 
 	//dereferencing (*empl) to get actual value
@@ -66,4 +73,17 @@ func main(){
 
 	//we can access the struct poiner without dereferencing it
 	fmt.Println(empl.fullTime)
+
+	//struct type of anonymous fields
+	type Data struct{
+		string
+		int
+		bool
+	} 
+
+	sample := Data{"viswa", 1000, false}
+	fmt.Println(sample.bool)
+
+	//field promotion - anonymous nested struct, all nested struct fields are automatically 
+	//available on parent struct
 }
